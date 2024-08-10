0fc7e18a7a4afc262424b478808ce28c37f8930b
Thorbert1 <tobyas2005139@gmail.com>
2024-08-11 00:06:33 +0700

    Encapsulated file submission area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,20 @@
+import FileSubmissionArea from "@/components/file-submission-area";
 import TestArea from "@/components/test-area";
 import Image from "next/image";
 
 export default function Home() {
-  const handleFormSubmit = (formData: FormData) => {
-    const rawFormData = {
-      text: formData.get("text"),
-      file: formData.get("file"),
-    };
-    const text = formData.get("text") as string;
-    console.log(text);
-  };
-
   return (
     <>
-      <div className="z-0 flex w-screen flex-grow-[1] flex-col space-y-5 p-3 md:p-5">
-        <div id="typing-area" className="flex-grow-[1] border border-black">
+      <div className="z-0 flex h-screen w-screen flex-col space-y-5 p-3 md:p-5">
+        <div id="typing-area" className="flex h-1/2 border border-black">
           <TestArea />
         </div>
-        <div className="flex flex-grow-[1] flex-row">
+        <div id="bottom" className="flex h-1/2 flex-row space-x-3 md:space-x-5">
           <div
             id="statistics"
-            className="flex-grow-[2] border border-black"
+            className="flex flex-1 border border-black"
           ></div>
-          <div
-            id="file submission"
-            className="flex flex-grow-[1] flex-col border border-black p-3 md:p-5"
-          >
-            <form action={handleFormSubmit}>
-              <input accept=".txt" type="file" name="text" id="text-submit" />
-              <button type="submit">Submit file for analysis</button>
-            </form>
-          </div>
+          <FileSubmissionArea />
         </div>
       </div>
     </>
---
+++ src/components/file-submission-area.tsx
@@ -0,0 +1,52 @@
+export default function FileSubmissionArea() {
+  const handleFormSubmit = async (formData: FormData) => {
+    "use server";
+    const rawFormData = {
+      text: formData.get("text"),
+      file: formData.get("file"),
+    };
+    const text = formData.get("text") as string;
+    console.log(text);
+  };
+
+  return (
+    <div
+      id="file submission"
+      className="flex flex-1 flex-col border border-black p-3 md:p-5"
+    >
+      <h1 className="text-xl">Submit a text</h1>
+      <form
+        action={handleFormSubmit}
+        className="flex h-full flex-col space-y-3 md:space-y-5"
+      >
+        <div
+          id="preview-box"
+          className="flex-[4] border border-black p-3 md:p-5"
+        >
+          <p>{}</p>
+        </div>
+        <div id="button-group" className="flex flex-row space-x-3 md:space-x-5">
+          <label
+            htmlFor="text-submit"
+            className="flex-1 cursor-pointer border border-black p-3 text-center md:p-5"
+          >
+            Browse files
+          </label>
+          <input
+            accept=".txt"
+            type="file"
+            name="text-submit"
+            id="text-submit"
+            className="hidden"
+          />
+          <button
+            type="submit"
+            className="flex-1 border border-black p-3 md:p-5"
+          >
+            Submit file for review
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
